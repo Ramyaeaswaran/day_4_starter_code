b51b24d881c1a798b3b16b74e5a8ce2f4f9e11ee
Ramyaeaswaran <ramyaeaswaran77@gmail.com>
2019-10-03 12:40:18 +0530

    Dice App

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,79 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'dart:math';
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.red,
-        appBar: AppBar(
-          title: Text('Roll the Dice'),
-          backgroundColor: Colors.red,
-        ),
-        body: Container(),
-      ),
+      home: DicePage(),
     ),
   );
 }
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftdicenum =1;
+  int rightdicenum=6;
+  void modifyleft(){
+    setState(() {
+        leftdicenum= 1+ Random().nextInt(6);
+        rightdicenum= 1+ Random().nextInt(6);
+    }
+      );
+  }
+ /* void modifyright(){
+    print('right');
+    setState(() {
+      rightdicenum= 1+ Random().nextInt(6);
+      leftdicenum= 1+ Random().nextInt(6);
+    });
+  }*/
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red,
+      appBar: AppBar(
+        title: Text('Roll the Dice'),
+        backgroundColor: Colors.red,
+      ),
+      body: Center(
+
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: FlatButton(
+                child: Image(
+                  image: AssetImage('assets/dice$leftdicenum.png'),
+                 // width: 80,
+                ),
+                onPressed: modifyleft,
+              ),
+            ),
+            Expanded(
+              child: FlatButton(
+                child: Image(
+                  image: AssetImage('assets/dice$rightdicenum.png'),
+                //  width: 80,
+                ),
+                onPressed: modifyleft,
+              ),
+            ),
+          ],
+        ),
+         ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: modifyleft,
+        child: Icon(Icons.refresh),
+        backgroundColor: Colors.redAccent,
+      ),
+    );
+  }
+}
+
+
+
+
+
